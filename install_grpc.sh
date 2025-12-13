9b98d4edee6ba1da12316405fd639094a61f0138
yiboyan@usc.edu <Yibo Yan>
2025-12-13 01:59:47 -0800

    mod: update build type

--- install_grpc.sh
+++ install_grpc.sh
@@ -8,22 +8,42 @@ sudo apt install -y cmake \
        	libtool \
        	pkg-config
 
-cd $HOME
-git clone --recurse-submodules -b v1.64.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
+function install_mqtt_c_lib {
+	TMP_FOLDER="/tmp/mosquitto"
+
+	rm -rf $TMP_FOLDER
+	mkdir -p $TMP_FOLDER
+	pushd $TMP_FOLDER
+	git clone https://github.com/eclipse/paho.mqtt.c.git
+	cd paho.mqtt.c
+	make -j$(nproc)
+	sudo make uninstall | true  # clean up first
+	sudo make install   # install mosquitto c lib
+	popd
+}
+
+install_mqtt_c_lib
 
 # Build and Install gRPC
 export MY_INSTALL_DIR=$HOME/.local
 mkdir -p $MY_INSTALL_DIR
 export PATH="$MY_INSTALL_DIR/bin:$PATH"
 
+pushd $HOME
+# git clone --recurse-submodules -b v1.64.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
+git clone https://github.com/USC-NSL-DDB/grpc.git
+
 cd grpc
+git submodule update --init --recursive
 mkdir -p cmake/build
-pushd cmake/build
-cmake -DgRPC_INSTALL=ON \
-      -DgRPC_BUILD_TESTS=OFF \
-      -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
-      ../..
-make -j$(nproc)
-make install
+cd cmake/build
+cmake -G Ninja \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DgRPC_INSTALL=ON \
+  -DgRPC_BUILD_TESTS=OFF \
+  -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
+  -DCMAKE_C_COMPILER=gcc-13 -DCMAKE_CXX_COMPILER=g++-13 \
+  ../..
+cmake --build . --target install -- -j$(nproc)
 popd
 
