e27b7416f0f3ca272c183ac5c79260a5e8317158
Yibo Yan <yiboyan@usc.edu>
2025-12-15 01:48:32 -0800

    mod: build script

--- build.sh
+++ build.sh
@@ -1,39 +1,7 @@
 #!/bin/bash
 
-# Arguments
-while getopts ":j:" opt; do
-  case $opt in
-    j) JOBS="$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    exit 1
-    ;;
-  esac
-
-  case $OPTARG in
-    -*) echo "Option $opt needs a valid argument"
-    exit 1
-    ;;
-  esac
-done
-
-# Environment variables
-set -x
-
-# GRPC build for python
-# python -m grpc_tools.protoc -Icsrc/protos --python_out=. --grpc_python_out=. csrc/protos/runtime.proto
-
-# CPP runtime build.
 mkdir -p build
 pushd build/
 cmake ..
-
-if [[ -z "$JOBS" ]]; then
-  echo "Jobs number is not specified. Running with make -j" 
-  make -j
-else
-  echo "Setting jobs number as $JOBS..."
-  make -j$JOBS
-fi
-
+make -j$(nproc)
 popd
